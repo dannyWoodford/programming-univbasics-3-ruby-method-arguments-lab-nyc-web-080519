5dd6cb63705ea5985d39ee9e81b16f29af999029
daniel Woodford <daniel.woodford.g@gmail.com>
2019-08-15 02:51:00 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,6 +7,9 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
 
+<<<<<<< HEAD
 def introduction_with_language_optional(name, language="Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+=======
+>>>>>>> 1bdc20915d9fd84bfd2595ac59b9c78f60cfa763
